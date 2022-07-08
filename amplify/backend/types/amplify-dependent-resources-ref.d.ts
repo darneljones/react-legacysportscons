b044fb59ac90684c7c5364a3947e28d5aa4a0291
ElectAmberCollins-GebrehiwetforJudge <darneljones.corp@gmail.com>
2022-07-08 00:43:12 -0400

    august-release-03: Enabled Auth and sign in screen.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "reactlegacysportscon1679409d": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "api": {
+        "reactlegacysportscon": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import "./index.css";
-import LoginScreen from "./components/LoginScreen/LoginScreen";
-import Loader from "./components/Loader/Loader";
+import "./App.css";
+// import LoginScreen from "./components/LoginScreen/LoginScreen";
+// import Loader from './components/Loader/Loader';
 import React, { useState, useEffect } from "react";
+import {
+  withAuthenticator,
+  AmplifySignOut,
+  Authenticator,
+} from "@aws-amplify/ui-react";
+import { Amplify, Auth } from "aws-amplify";
 
-export default function App() {
-  const [loading, setLoading] = useState(true);
+import awsconfig from "./aws-exports";
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
+Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
+
+function App() {
+  // const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2000);
+  // }, []);
   return (
-    <>
-      {loading === false ? (
-        <div>
-          <LoginScreen />
-        </div>
-      ) : (
-        <Loader />
-      )}
-    </>
+    <div className="App">
+      <header className="App-header">
+        <AmplifySignOut />
+        <h2>My App Content</h2>
+      </header>
+    </div>
   );
 }
+export default withAuthenticator(App);
